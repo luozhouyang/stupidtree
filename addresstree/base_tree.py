706b561ae6b5b4471ad2e12b941ee1c29dcaa4f5
luozhouyang <stupidme.me.lzy@gmail.com>
2018-06-28 22:34:29 +0800

    Add doc and create nodes dynamically

--- addresstree/base_tree.py
+++ addresstree/base_tree.py
@@ -1,37 +1,68 @@
 import abc
 
-from .level import Level
 from .node import AddressNode
 
 
 class BaseTreeInterface(abc.ABC):
+    """A interface defines basic abilities a tree has."""
 
     @abc.abstractmethod
     def put(self, words):
+        """Put a list of string or a single SPACE separated string to tree.
+
+        :param words: If it is a list of string, each word in that will be inserted
+                into tree. If it is a SPACE separated string, it will be split
+                to a list of string and then insert these words to the tree.
+        :return:
+        """
         raise NotImplementedError()
 
     @abc.abstractmethod
     def get(self, key):
+        """Get all the nodes in the tree whose `data` field equals the `key`.
+
+        :param key: The key to match.
+        :return: A list of `AddressNode` objects if found, or empty list.
+        """
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def remove(self, key, callback):
+    def remove(self, key, rm_filter=None):
+        """Remove nodes whose `data` field euqls the `key` from the tree.
+
+        If not found any node by the `key` in the tree, then do noting.
+
+        If found node(s), the caller can decide whether remove the node or not
+        using the `rm_filter` argument, whose signature is:
+        ```python
+        def callback(node)
+            if ...:
+                return True
+            return False
+        ```
+        If `callback` returns `True`, the node will be removed,
+        or returns `False', the node will be reserved.
+
+        If `callback` is None, each node matched will be removed.
+        :param key: The key to match.
+        :param rm_filter: A callback function to decide whether remove the node
+            or not.This is helpful when there has multi nodes that
+            match the `key`.
+        :return:
+        """
         raise NotImplementedError()
 
     @abc.abstractmethod
     def size(self):
+        """The total number of nodes in the tree."""
         raise NotImplementedError()
 
 
 class BaseTree(BaseTreeInterface):
 
-    def __init__(self, level_offset=2, root=None):
-        self.level_offset = level_offset
-        if root is None:
-            self.root = AddressNode("", None, None)
-        else:
-            self.root = root
-        self.nodes_count = 1
+    def __init__(self):
+        self.root = None
+        self.nodes_count = 0
 
     def put(self, words):
         if isinstance(words, list):
@@ -42,9 +73,15 @@ class BaseTree(BaseTreeInterface):
             raise TypeError("Argument `words` must be in str or list Type.")
 
     def _put(self, node, words, depth):
+        if not node:
+            node = self._create_root_node(words, depth)
+            assert node
+            self.on_insert(node)
         if depth == len(words):
             return
-        child = AddressNode(words[depth], Level(depth + 2), node)
+        # child = AddressNode(words[depth], Level(depth + 2), node)
+        child = self._create_node(node, words, depth)
+        assert isinstance(child, AddressNode)
         for c in node.children:
             if c == child:
                 self._put(c, words, depth + 1)
@@ -54,21 +91,47 @@ class BaseTree(BaseTreeInterface):
         self._put(child, words, depth + 1)
 
     @abc.abstractmethod
+    def _create_root_node(self, words, depth):
+        """Create the root node.
+
+        :param words: words that to be inserted
+        :param depth: index of current word in words
+        :return: A root node. Can not be NoneType
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def _create_node(self, node, words, depth):
+        """Create node
+
+        :param node: parent node
+        :param words: words to be inserted
+        :param depth: index of current word in words
+        :return: A node. Can not be NoneType
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
     def on_insert(self, node):
+        """Notify node insertion
+
+        :param node: the node to be inserted.
+        :return:
+        """
         raise NotImplementedError()
 
     @abc.abstractmethod
     def get(self, key):
         raise NotImplementedError()
 
-    def remove(self, key, callback):
+    def remove(self, key, rm_filter=None):
         if not key:
             return
         nodes = self.get(key)
         if not nodes or len(nodes) == 0:
             return
         for n in nodes:
-            if callback(n):
+            if rm_filter is None or rm_filter(n):
                 self._remove(n)
 
     def _remove(self, n):
@@ -84,6 +147,11 @@ class BaseTree(BaseTreeInterface):
 
     @abc.abstractmethod
     def on_remove(self, node):
+        """Notify node remove
+
+        :param node: the node to be removed
+        :return:
+        """
         raise NotImplementedError()
 
     def size(self):
