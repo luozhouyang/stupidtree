830a5c072e2974253a0fe892253e9568215f37a9
luozhouyang <stupidme.me.lzy@gmail.com>
2018-06-28 22:54:07 +0800

    Refactor indexed tree

--- addresstree/indexed_tree.py
+++ addresstree/indexed_tree.py
@@ -1,25 +1,29 @@
 from .base_tree import BaseTree
 from .indexer import NodeIndexer
+import abc
 
 
 class IndexedTree(BaseTree):
 
-    def __init__(self,
-                 insert_callback=None,
-                 remove_callback=None,
-                 level_offset=2, root=None):
-        self.insert_callback = insert_callback
-        self.remove_callback = remove_callback
+    def __init__(self, ):
+        super().__init__()
         self.indexer = NodeIndexer()
-        super().__init__(level_offset, root)
 
     def on_insert(self, node):
         self.nodes_count += 1
-        self.indexer.put(node.data, node, self.insert_callback)
+        self.indexer.put(node.data, node)
 
     def get(self, key):
         return self.indexer.get(key)
 
     def on_remove(self, node):
         self.nodes_count -= 1
-        self.indexer.remove(node.data, self.remove_callback)
+        self.indexer.remove(node)
+
+    @abc.abstractmethod
+    def _create_root_node(self, words, depth):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def _create_node(self, node, words, depth):
+        raise NotImplementedError()
