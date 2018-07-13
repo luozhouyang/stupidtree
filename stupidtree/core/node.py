af634b3dea35df7fb6b3cda4804b27b8ec28638b
luozhouyang <stupidme.me.lzy@gmail.com>
2018-07-13 19:35:57 +0800

    Format file

--- stupidtree/core/node.py
+++ stupidtree/core/node.py
@@ -18,6 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
 class Node(object):
     def __init__(self, data, tag, parent):
         self.data = data
